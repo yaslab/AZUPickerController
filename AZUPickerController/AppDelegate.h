079c06ba6c3755cc1d252062c71bb859cf596d39
Yasuhiro Hatta <hatta.yasuhiro@gmail.com>
2015-02-12 01:59:25 +0900

    Initial Commit

---
+++ AZUPickerController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AZUPickerController
+//
+//  Created by Yasuhiro Hatta on 2015/02/12.
+//  Copyright (c) 2015年 yaslab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AZUPickerController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AZUPickerController
+//
+//  Created by Yasuhiro Hatta on 2015/02/12.
+//  Copyright (c) 2015年 yaslab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
